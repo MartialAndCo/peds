faa84b946050bdfaee9df61aaccbca729fce188d
VISUARA <98740111+visuarafr@users.noreply.github.com>
2026-01-09 00:02:20 +0100

    fix: allow media-src data and blob in CSP

--- next.config.ts
+++ next.config.ts
@@ -20,7 +20,7 @@ const nextConfig: NextConfig = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' blob:; style-src 'self' 'unsafe-inline'; font-src 'self' data:; img-src 'self' data: https: blob:; worker-src 'self' blob:; connect-src 'self' https:;",
+            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' blob:; style-src 'self' 'unsafe-inline'; font-src 'self' data:; img-src 'self' data: https: blob:; media-src 'self' data: blob:; worker-src 'self' blob:; connect-src 'self' https:;",
           },
         ],
       },
